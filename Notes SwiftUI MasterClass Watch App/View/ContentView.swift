7112147d44b6e52274f13b4c0b57dabf3a20b3d0
Aran Fononi <aaronfononi@gmail.com>
2025-04-18 15:35:09 +0330

    Add Save Delete and Load functions to the note app

--- Notes SwiftUI MasterClass Watch App/View/ContentView.swift
+++ Notes SwiftUI MasterClass Watch App/View/ContentView.swift
@@ -13,8 +13,39 @@ struct ContentView: View {
     @State private var text: String = ""
     
     // MARK: - Function
+    func getDocumentsDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     func save() {
-        dump(notes)
+        //dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentsDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Saving data failed")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentsDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                print("Loading data failed")
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
     }
     
     var body: some View {
@@ -44,10 +75,40 @@ struct ContentView: View {
                     .buttonStyle(PlainButtonStyle())
                     .foregroundStyle(.accent)
                 } //: HStack
+                
                 Spacer()
-                Text("\(notes.count) notes")
+                
+                if notes.count >= 1 {
+                    List {
+                        ForEach(notes) { note in
+                            HStack {
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundStyle(.accent)
+                                Text(note.text)
+                                    .lineLimit(1)
+                                    .padding(.leading, 5)
+                            } //: HStack
+                        } //: Loop
+                        .onDelete(perform: delete)
+                        
+                    }
+                } else {
+                    Spacer()
+                    Image(systemName: "note.text")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.gray)
+                        .opacity(0.25)
+                        .padding(25)
+                    Spacer()
+                } //: List
+                
             } //: VStack
-            .navigationTitle("Notes")
+            .navigationTitle("\(notes.count) Notes")
+            .onAppear {
+                load()
+            }
         } //: Navigation
         
         
