8feb8471f21f7ba567c4b29a66746636fb6f4678
Aran Fononi <aaronfononi@gmail.com>
2025-04-18 14:40:23 +0330

    Create Note Model

---
+++ Notes SwiftUI MasterClass Watch App/Model/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Notes SwiftUI MasterClass Watch App
+//
+//  Created by Aran Fononi on 18/4/25.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+    let id: UUID
+    let text: String
+}
