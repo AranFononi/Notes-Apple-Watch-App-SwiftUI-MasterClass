f25937992daa987146568355015e025fc4b5927e
Aran Fononi <aaronfononi@gmail.com>
2025-04-18 14:47:40 +0330

    Add TextField

--- Notes SwiftUI MasterClass Watch App/View/ContentView.swift
+++ Notes SwiftUI MasterClass Watch App/View/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Property
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+    
+    // MARK: - Function
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack(alignment: .center, spacing: 6) {
+                TextField("Add New Note", text: $text)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                        
+                }
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
+                .foregroundStyle(.accent)
+            } //: HStack
+            Spacer()
+        } //: VStack
+        .navigationTitle(Text("Notes"))
+        
     }
 }
 
