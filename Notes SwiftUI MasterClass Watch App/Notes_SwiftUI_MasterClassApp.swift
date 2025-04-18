e30719efc139623b1bed94b798c9421c2c601c17
Aran Fononi <aaronfononi@gmail.com>
2025-04-18 14:33:39 +0330

    Initial Commit

---
+++ Notes SwiftUI MasterClass Watch App/Notes_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_SwiftUI_MasterClassApp.swift
+//  Notes SwiftUI MasterClass Watch App
+//
+//  Created by Aran Fononi on 18/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Notes_SwiftUI_MasterClass_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
